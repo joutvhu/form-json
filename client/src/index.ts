41e659c4436116b798bbd1bb4412313b7cd51a5f
Giao Ho <joutvhu@gmail.com>
2023-05-05 14:18:49 +0700

    deserializeFormJson

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,7 +2,7 @@ export interface FileMap {
   [name: string]: Blob;
 }
 
-export function serializeFormJson(data: any): FormData | undefined {
+export function serializeFormJson<T>(data: T): FormData | undefined {
   if (data == null)
     return undefined;
   let index = 0;
@@ -27,4 +27,27 @@ export function serializeFormJson(data: any): FormData | undefined {
     form.append(key, files[key]);
   }
   return form;
+}
+
+export function deserializeFormJson<T>(data: FormData): T | undefined {
+  if (data == null)
+    return undefined;
+  const json: any = data.get('data');
+  if (json == null)
+    return undefined;
+  if (typeof json !== 'string')
+    throw new Error('The data is not a JSON string');
+  const value = JSON.parse(json, (_key: string, value: any) => {
+    if (typeof value === 'string') {
+      if (/^file_\d$/.test(value)) {
+        const file = data.get(value);
+        if (file != null)
+          return file;
+        else
+          return value;
+      }
+    }
+    return value;
+  });
+  return value as T;
 }
--- dotNet/RestJsonData/FormJsonModelBinder.cs
+++ dotNet/RestJsonData/FormJsonModelBinder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Newtonsoft.Json;
 
 namespace Joutvhu.AspNet.RestJsonData
