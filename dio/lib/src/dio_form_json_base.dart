c5e4eaa4a5b2ca788b22e89864b735fe6ccc65a1
Giao Ho <joutvhu@gmail.com>
2023-06-22 16:35:46 +0700

    Add DioFormJsonInterceptor

--- dio/lib/src/dio_form_json_base.dart
+++ dio/lib/src/dio_form_json_base.dart
@@ -3,29 +3,42 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 
-FormData toFormJson(
-  Object value, [
-  bool camelCaseContentDisposition = false,
-]) {
-  var result = FormData(
-    camelCaseContentDisposition: camelCaseContentDisposition,
-  );
-  int index = 0;
-  var data = json.encode(
-    value,
-    toEncodable: (object) {
-      if (object is File) {
-        object = MultipartFile.fromFile(object.path);
+class FormJson extends FormData {
+  FormJson.fromObject(
+    Object value, [
+    bool camelCaseContentDisposition = false,
+  ]) : super(camelCaseContentDisposition: camelCaseContentDisposition) {
+    int index = 0;
+    var data = json.encode(
+      value,
+      toEncodable: (object) {
+        if (object is File) {
+          object = MultipartFile.fromFile(object.path);
+        }
+        if (object is MultipartFile) {
+          var key = 'file_$index';
+          index++;
+          files.add(MapEntry(key, object));
+          return key;
+        }
+        return null;
+      },
+    );
+    fields.add(MapEntry('data', data ?? ''));
+  }
+}
+
+class DioFormJsonInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (options.contentType == 'multipart/form-json') {
+      options.contentType = 'multipart/form-data';
+      if (options.data != null) {
+        options.data = FormJson.fromObject(options.data);
       }
-      if (object is MultipartFile) {
-        var key = 'file_$index';
-        index++;
-        result.files.add(MapEntry(key, object));
-        return key;
-      }
-      return null;
-    },
-  );
-  result.fields.add(MapEntry('data', data ?? ''));
-  return result;
+    } else if (options.extra['form-json'] == true && options.data != null) {
+      options.data = FormJson.fromObject(options.data);
+    }
+    handler.next(options);
+  }
 }
